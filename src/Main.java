3c0abc04683b01119edebd43e5d4f132eb05ca4e
Seregakras <Seregakras@yandex.ru>
2022-08-28 15:18:44 +0300

    Start code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\tMy Calculator\n");
+        System.out.print("Enter your expression: ");
+        Scanner scan = new Scanner(System.in);
+        String Expression = scan.nextLine();
+        String [] strings =  Expression.split(" ");
+
+
+    }
+}
